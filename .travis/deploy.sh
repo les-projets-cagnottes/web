86b4d685a7390c9d1d7b69a905df2c746c5d40ca
Thomah <5090230+Thomah@users.noreply.github.com>
2019-09-18 19:00:03 +0200

    Make scp recursive

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,12 +6,11 @@ eval "$(ssh-agent -s)"
 chmod 600 $TRAVIS_BUILD_DIR/.travis/id_rsa
 ssh-add $TRAVIS_BUILD_DIR/.travis/id_rsa
 
-echo "Hello, world !"
-
 ssh-keyscan -t rsa -H $IP >> ~/.ssh/known_hosts
-scp $TRAVIS_BUILD_DIR/dist/* apps@$IP:$DEPLOY_DIR/dist/
+scp -rp $TRAVIS_BUILD_DIR/dist/* apps@$IP:$DEPLOY_DIR/dist/
 
 ssh -p $PORT apps@$IP -o StrictHostKeyChecking=no "$( cat <<EOT
+    cd $DEPLOY_DIR
     echo "$(date -u) Travis Deploy"  >> ./console.log
     exit
 EOT
