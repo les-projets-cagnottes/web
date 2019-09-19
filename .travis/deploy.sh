58ec2b3dcda08bbf60fe844ca904dfad6fb0420f
Thomah <5090230+Thomah@users.noreply.github.com>
2019-09-19 18:06:43 +0200

    Test Travis deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -9,12 +9,12 @@ ssh-add $TRAVIS_BUILD_DIR/.travis/id_rsa
 ssh-keyscan -t rsa -H $IP >> ~/.ssh/known_hosts
 
 ssh -p $PORT apps@$IP -o StrictHostKeyChecking=no "$( cat <<EOT
-    rm -rf $DEPLOY_DIR/dist
+    rm -rf $DEPLOY_DIR/dist/*
     exit
 EOT
 )"
 
-scp -rp $TRAVIS_BUILD_DIR/dist apps@$IP:$DEPLOY_DIR
+scp -rp $TRAVIS_BUILD_DIR/dist/* apps@$IP:$DEPLOY_DIR
 
 ssh -p $PORT apps@$IP -o StrictHostKeyChecking=no "$( cat <<EOT
     cd $DEPLOY_DIR
