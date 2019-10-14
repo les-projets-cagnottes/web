524fd05d096d7264b03daad8ee8a01147617cbcb
Thomah <5090230+Thomah@users.noreply.github.com>
2019-10-14 18:17:07 +0200

    Set https url for production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://15.188.32.86:8080/api',
+  apiUrl: 'https://les-projets-cagnottes.fr/api',
   slackClientId: '115462816321.761013265159'
 };
