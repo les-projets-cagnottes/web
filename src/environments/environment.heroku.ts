65d68e9d73f492c5ec9b305bb06983175fa29ec9
Thomah <5090230+Thomah@users.noreply.github.com>
2020-05-22 21:01:17 +0200

    Change Slack Client ID to test 0.5 migration

--- src/environments/environment.heroku.ts
+++ src/environments/environment.heroku.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://les-projets-cagnottes-api.herokuapp.com/api',
-  slackClientId: '744027460679.730717520259'
+  slackClientId: '115462816321.761013265159'
 };
