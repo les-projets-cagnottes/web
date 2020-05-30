e3954039d7a7fb9b2a7f8519e042cc57810c3ce6
Thomah <5090230+Thomah@users.noreply.github.com>
2020-05-30 17:39:48 +0200

    Update Heroku environment Slack Client ID

--- src/environments/environment.heroku.ts
+++ src/environments/environment.heroku.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://les-projets-cagnottes-api.herokuapp.com/api',
-  slackClientId: '115462816321.761013265159',
+  slackClientId: '744027460679.730717520259',
   gitRef: 'master'
 };
