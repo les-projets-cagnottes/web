70ce5090ec25c1e584ffc9a3575ad369b01a7e60
Thomah <5090230+Thomah@users.noreply.github.com>
2021-12-30 17:46:56 +0100

    Remove contributor from donation model and remove unused files

--- src/app/_models/donation/donation.model.ts
+++ src/app/_models/donation/donation.model.ts
@@ -3,6 +3,5 @@ import { AuditModel, GenericModel } from "..";
 export class DonationModel extends AuditModel {
     amount: number;
     account: GenericModel;
-    contributor: GenericModel;
     campaign: GenericModel;
 }
