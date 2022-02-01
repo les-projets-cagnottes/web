4fd1b26eb0674bb106321415240470f53540b9b8
Thomah <5090230+Thomah@users.noreply.github.com>
2022-02-01 19:07:33 +0100

    Display Microsoft organization name

--- src/app/_models/ms-team/ms-team.model.ts
+++ src/app/_models/ms-team/ms-team.model.ts
@@ -1,6 +1,8 @@
 import { AuditModel, GenericModel } from "..";
 
 export class MsTeamModel extends AuditModel {
+    displayName: string = '';
     tenantId: string = '';
+    groupId: string = '';
     organization: GenericModel = new GenericModel();
 }
