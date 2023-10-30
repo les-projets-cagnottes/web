106075145bfe517acd16426a2c3e0a597906e177
Thomah <5090230+Thomah@users.noreply.github.com>
2023-10-30 20:44:53 +0100

    fix: display of rules, publishing ideas from scratch, resolve project refresh on first login and complete documentation

--- src/app/_services/authentication/authentication.service.ts
+++ src/app/_services/authentication/authentication.service.ts
@@ -20,7 +20,7 @@ export class AuthenticationService {
     public currentOrganization: Observable<Organization>;
 
     constructor(
-        private http: HttpClient, 
+        private http: HttpClient,
         private router: Router,
         private configService: ConfigService) {
         const userInLocalStorage = localStorage.getItem('currentUser');
